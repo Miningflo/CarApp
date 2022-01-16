403f6324f3df8b42eb29b24b17b726f45e918c3d
Miningflo <debosscher.flore@gmail.com>
2022-01-16 03:48:57 +0100

    commit

--- location.js
+++ location.js
@@ -24,7 +24,7 @@ function street_n_speed(namebox, maxspeed){
                     maxspeed.style.visibility = "visible";
                     maxspeed.innerText = "" + tags["maxspeed"];
                 }else{
-                    maxspeed.style.visibility = "hidden";
+                    maxspeed.style.visibility = "visible";
                 }
             })
             .catch(err => console.error(err))
